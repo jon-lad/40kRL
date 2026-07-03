681b5e84852a35a5ecb4465d1da62cb7ed983528
jon-lad <jon_lad@hotmail.com>
2026-07-03 22:01:14 +0100

    Hit chance: implement constructor, clampSkill, defaultRoll, and modifier methods

--- Source/Attacker.cpp
+++ Source/Attacker.cpp
@@ -1,8 +1,47 @@
 
 #include <sstream>
+#include <random>
+#include <algorithm>
+#include <numeric>
 #include "main.h"
 
-Attacker::Attacker(float power) : power{ power } {}
+Attacker::Attacker(float power, int skillValue)
+	: power{ power }
+	, skillValue{ clampSkill(skillValue) }
+	, rollD100{ defaultRoll }
+{
+}
+
+int Attacker::clampSkill(int value) {
+	return std::max(MIN_SKILL, std::min(MAX_SKILL, value));
+}
+
+int Attacker::defaultRoll() {
+	static std::mt19937 engine{ std::random_device{}() };
+	static std::uniform_int_distribution<int> dist(1, 100);
+	return dist(engine);
+}
+
+int Attacker::computeThreshold() const {
+	int sum = std::accumulate(modifiers.begin(), modifiers.end(), 0);
+	int raw = skillValue + sum;
+	return std::max(MIN_SKILL, std::min(MAX_SKILL, raw));
+}
+
+void Attacker::addModifier(int mod) {
+	modifiers.push_back(mod);
+}
+
+void Attacker::removeModifier(int mod) {
+	auto it = std::find(modifiers.begin(), modifiers.end(), mod);
+	if (it != modifiers.end()) {
+		modifiers.erase(it);
+	}
+}
+
+void Attacker::clearModifiers() {
+	modifiers.clear();
+}
 
 void Attacker::attack(Actor* owner, Actor* target)
 {
